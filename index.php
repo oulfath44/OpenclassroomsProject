894de6cbda67162776584140ba80a5fe7d0fb220
Oulfath AMOUSSA <oulfathamoussa4@gmail.com>
2024-06-17 12:16:41 +0200

    Ajout du contenu dans le fichier

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Bienvenue dans mon espace</h2>
+    <ul>
+        <li>Hello</li>
+        <li>Oulfath</li>
+        <li>Monyossola</li>
+    </ul>
+</body>
+</html>
